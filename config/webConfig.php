7116ad010d04a1dbdeed34bc34f98a34024eed6b
Fong Shi Wei <shiweifong@gmail.com>
2015-05-06 17:50:10 +0800

    production release

--- config/webConfig.php
+++ config/webConfig.php
@@ -2,7 +2,7 @@
     class webconfig{
         public function webconfig(){
             $webConfigObj = new stdClass();
-            $webConfigObj->production = false;
+            $webConfigObj->production = true;
             $webConfigObj->cloudServer = 'http://www.eyexcess.com';
 //            $webConfigObj->cloudServer = 'http://localhost:4000';
 
